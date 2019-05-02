cceb64e4c82a65558de8bd9a80fbda627052d792
Nikola Samardzic <nikola.s@ucla.edu>
2019-05-02 15:12:00 -0700

    Works

--- verilog/CONTROL.v
+++ verilog/CONTROL.v
@@ -43,6 +43,7 @@ module CONTROL(input i_clk,
    
    always @(i_a_empty or i_b_empty or i_r_a_min_zero or i_r_b_min_zero or i_a_min_zero or i_b_min_zero or state)
      begin
+	#0.1;   
 	casez(state)
 	  TOGGLE: begin
 	     if (i_a_empty) 
