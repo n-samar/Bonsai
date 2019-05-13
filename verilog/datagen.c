1c275ffbc8563169065f29005d45bd51b0d7adc9
Nikola Samardzic <nikola@risksciences.ucla.edu>
2019-05-12 22:04:26 -0700

    Implemented coupler; still should worry about getting an odd number of zeros on inputs

--- verilog/datagen.c
+++ verilog/datagen.c
@@ -20,20 +20,23 @@ int main(int argc, char ** argv) {
     int records_per_tuple = (argc>=5) ? atoi(argv[4]) : 1;    
     int records_per_way = atoi(argv[2]) * records_per_tuple;
     int seed = (argc>=4) ? atoi(argv[3]) : 1; // random gen seed
+    int out_records_per_tuple = (argc>=6) ? atoi(argv[5]) : records_per_tuple;
 
     int n = ways * records_per_way;
     int i, j;
     printf("datagen %d way, %d records_per_way.\n", ways, records_per_way);
 
     uint32_t *buf;
-    buf = calloc(n, sizeof(uint32_t));
+    buf = calloc(n+1, sizeof(uint32_t));
 
     srand(seed);
 
     /******* generate keys *****/
     for (i=0; i<n; i++) buf[i] = rand();
     for (i=0; i<records_per_tuple; i++) {
-      buf[i] = 0;   // Need a zero input
+        if (out_records_per_tuple == records_per_tuple) {
+            buf[i] = 0;   // Need a zero input
+        }
     }
    
     
@@ -67,7 +70,9 @@ int main(int argc, char ** argv) {
 	qsort(buf+i*records_per_way,
 	      records_per_way, sizeof(uint32_t), comp);
 	for (int j=0; j<records_per_tuple; j++) {
-	  buf[i*records_per_way+j] = 0;
+        if (out_records_per_tuple == records_per_tuple) {
+            buf[i*records_per_way+j] = 0;
+        }
 	}
       }
       for (i=0; i<n; i++){
@@ -77,8 +82,11 @@ int main(int argc, char ** argv) {
 	  fprintf(dat, "%08x", buf[i+records_per_tuple-1-2*(i%records_per_tuple)]);	  
 	}
       }
-      for (i=0; i<records_per_tuple; i++) {
-	fprintf(dat, "%08x", 0);   // Add trailing zero for testing convenience
+      for (i=0; i<out_records_per_tuple; i++) {
+          fprintf(dat, "%08x", 0);   // Add trailing zero for testing convenience
+          if (i%records_per_tuple == 0) {
+              fprintf(dat, "\n");
+          }
       }
       fprintf(dat, "\n");
       fclose(dat);
@@ -99,10 +107,10 @@ int main(int argc, char ** argv) {
       strcat(ans_filename, ".txt");            
       FILE *ans = fopen(ans_filename, "w+");
       for (i=0; i<n; i++){
-	if (i%records_per_tuple == records_per_tuple-1) {
-	  fprintf(ans, "%08x\n", buf[i-records_per_tuple+1]);
+	if (i%out_records_per_tuple == out_records_per_tuple-1) {
+	  fprintf(ans, "%08x\n", buf[i-out_records_per_tuple+1]);
 	} else {
-	  fprintf(ans, "%08x", buf[i+records_per_tuple-1-2*(i%records_per_tuple)]);	  
+	  fprintf(ans, "%08x", buf[i+out_records_per_tuple-1-2*(i%out_records_per_tuple)]);	  
 	}
       }
       fclose(ans);
