0ad9cc7fabefb45ab4e64ed7eca91824cc0ec7c0
Nikola Samardzic <nikola.s@ucla.edu>
2019-05-16 12:12:28 -0700

    Implemented Tree P2 L8

--- verilog/MERGER_TREE_P2_L4.v
+++ verilog/MERGER_TREE_P2_L4.v
@@ -1,7 +1,7 @@
 `timescale 1 ns/10 ps
 
 
-module MERGER_TREE_P1_L4 #(parameter L = 4) (input i_clk,
+module MERGER_TREE_P2_L4 #(parameter L = 4) (input i_clk,
 					   input [32*2*L-1:0] 	i_fifo,
 					   input [2*L-1:0] 	i_fifo_empty,
 					   input 		i_fifo_out_ready,
--- verilog/MERGER_TREE_P2_L8_ALL_MERGERS_2.v
+++ verilog/MERGER_TREE_P2_L8_ALL_MERGERS_2.v
@@ -1,31 +1,33 @@
 `timescale 1 ns/10 ps
 
 
-module MERGER_TREE_P1_L8 #(parameter L = 8) (input i_clk,
-					   input [32*2*L-1:0]   i_fifo,
+module MERGER_TREE_P2_L8 #(parameter L = 8) (input i_clk,
+					   input [DATA_WIDTH*2*L-1:0]   i_fifo,
 					   input [2*L-1:0]      i_fifo_empty,
 					   input 	      i_fifo_out_ready,
 					   output [2*L-1:0]     o_fifo_read, 
 					   output 	      o_out_fifo_write,
-					   output wire [2*32-1:0] o_data);
+					   output wire [DATA_WIDTH-1:0] o_data);
    parameter log_L = 3;
+   parameter DATA_WIDTH = 64;
+   
 
-   wire [31:0] 						      fifo_o_item_3 [7:0];
-   wire [31:0] 						      fifo_i_item_3 [7:0];   
+   wire [DATA_WIDTH-1:0] 						      fifo_o_item_3 [7:0];
+   wire [DATA_WIDTH-1:0] 						      fifo_i_item_3 [7:0];   
    wire [7:0] 						      fifo_read_3;
    wire [7:0] 						      fifo_write_3;
    wire [7:0] 						      fifo_empty_3;
    wire [7:0] 						      fifo_full_3;      
 
-   wire [32-1:0] 					      fifo_o_item_2 [3:0];
-   wire [31:0] 						      fifo_i_item_2 [3:0];   
+   wire [DATA_WIDTH-1:0] 						      fifo_o_item_2 [3:0];
+   wire [DATA_WIDTH-1:0] 						      fifo_i_item_2 [3:0];   
    wire [3:0] 						      fifo_read_2;
    wire [3:0] 						      fifo_write_2;
    wire [3:0] 						      fifo_empty_2;
    wire [3:0] 						      fifo_full_2;
 
-   wire [2*32-1:0] 						      fifo_o_item_1 [1:0];
-   wire [31:0] 						      fifo_i_item_1 [1:0];   
+   wire [DATA_WIDTH-1:0] 						      fifo_o_item_1 [1:0];
+   wire [DATA_WIDTH-1:0] 						      fifo_i_item_1 [1:0];   
    wire [1:0] 						      fifo_read_1;
    wire [1:0] 						      fifo_write_1;
    wire [1:0] 						      fifo_empty_1;
@@ -37,17 +39,17 @@ module MERGER_TREE_P1_L8 #(parameter L = 8) (input i_clk,
       for (level = L; level > 1; level = level/2) begin : IN
 	 for (i = 0; i < level; i=i+1) begin
 	    if (level == 8) begin
-	       MERGER_1 merger(.i_clk(i_clk),
-			     .i_fifo_1(i_fifo[32*2*i+31:32*2*i]),
+	       MERGER_2 merger(.i_clk(i_clk),
+			     .i_fifo_1(i_fifo[DATA_WIDTH*2*i+DATA_WIDTH-1:DATA_WIDTH*2*i]),
 			     .i_fifo_1_empty(i_fifo_empty[2*i]),
-			     .i_fifo_2(i_fifo[32*2*i+32+31:32*2*i+32]),
+			     .i_fifo_2(i_fifo[DATA_WIDTH*2*i+DATA_WIDTH+DATA_WIDTH-1:DATA_WIDTH*2*i+DATA_WIDTH]),
 			     .i_fifo_2_empty(i_fifo_empty[2*i+1]),
 			     .i_fifo_out_ready(~fifo_full_3[i] | fifo_read_3[i]),
 			     .o_fifo_1_read(o_fifo_read[2*i]),
 			     .o_fifo_2_read(o_fifo_read[2*i+1]),
 			     .o_out_fifo_write(fifo_write_3[i]),
 			     .o_data(fifo_i_item_3[i]));
-	       IFIFO16 fifo(.i_clk(i_clk),
+	       IFIFO16 #(DATA_WIDTH) fifo(.i_clk(i_clk),
 			    .i_data(fifo_i_item_3[i]),
 			    .i_enq(fifo_write_3[i]),
 			    .i_deq(fifo_read_3[i]),
@@ -56,7 +58,7 @@ module MERGER_TREE_P1_L8 #(parameter L = 8) (input i_clk,
 			    .o_full(fifo_full_3[i]));	       
 	    end
 	    else if (level == 4) begin
-	       MERGER_1 merger(.i_clk(i_clk),
+	       MERGER_2 merger(.i_clk(i_clk),
 			     .i_fifo_1(fifo_o_item_3[2*i]),
 			     .i_fifo_1_empty(fifo_empty_3[2*i]),
 			     .i_fifo_2(fifo_o_item_3[2*i+1]),
@@ -66,7 +68,7 @@ module MERGER_TREE_P1_L8 #(parameter L = 8) (input i_clk,
 			     .o_fifo_2_read(fifo_read_3[2*i+1]),
 			     .o_out_fifo_write(fifo_write_2[i]),
 			     .o_data(fifo_i_item_2[i]));
-	       IFIFO16 fifo(.i_clk(i_clk),
+	       IFIFO16 #(DATA_WIDTH) fifo(.i_clk(i_clk),
 			    .i_data(fifo_i_item_2[i]),
 			    .i_enq(fifo_write_2[i]),
 			    .i_deq(fifo_read_2[i]),
@@ -75,7 +77,7 @@ module MERGER_TREE_P1_L8 #(parameter L = 8) (input i_clk,
 			    .o_full(fifo_full_2[i]));
 	    end
 	    else if (level == 2) begin
-	       MERGER_1 merger(.i_clk(i_clk),
+	       MERGER_2 merger(.i_clk(i_clk),
 			     .i_fifo_1(fifo_o_item_2[2*i]),
 			     .i_fifo_1_empty(fifo_empty_2[2*i]),
 			     .i_fifo_2(fifo_o_item_2[2*i+1]),
@@ -85,7 +87,7 @@ module MERGER_TREE_P1_L8 #(parameter L = 8) (input i_clk,
 			     .o_fifo_2_read(fifo_read_2[2*i+1]),
 			     .o_out_fifo_write(fifo_write_1[i]),
 			     .o_data(fifo_i_item_1[i]));
-	       COUPLER #(32) fifo(.i_clk(i_clk),
+	       IFIFO16 #(DATA_WIDTH) fifo(.i_clk(i_clk),
 			    .i_data(fifo_i_item_1[i]),
 			    .i_enq(fifo_write_1[i]),
 			    .i_deq(fifo_read_1[i]),
@@ -113,10 +115,10 @@ endmodule
 			 
 		       
 /*
- 	       MERGER_1 merger(.i_clk(i_clk),
-			     .i_fifo_1(fifo_o_item[depth+1][32*2*i+31:32*2*i]),
+ 	       MERGER_2 merger(.i_clk(i_clk),
+			     .i_fifo_1(fifo_o_item[depth+1][DATA_WIDTH*2*i+DATA_WIDTH-1:DATA_WIDTH*2*i]),
 			     .i_fifo_1_empty(fifo_empty[depth+1][2*i]),
-			     .i_fifo_2(fifo_o_item[depth+1][32*2*i+32+31:32*2*i+32]),
+			     .i_fifo_2(fifo_o_item[depth+1][DATA_WIDTH*2*i+DATA_WIDTH+DATA_WIDTH-1:DATA_WIDTH*2*i+DATA_WIDTH]),
 			     .i_fifo_2_empty(fifo_empty[depth+1][2*i+1]),
 			     .i_fifo_out_ready(~fifo_full[depth][i] | fifo_ready[depth][i]),
 			     .o_fifo_1_read(fifo_read[depth+1][2*i]),
@@ -124,7 +126,7 @@ endmodule
 			     .o_out_fifo_write(fifo_write[depth][i]),
 			     .o_data(fifo_i_item[depth][i])
 			     );	 
-	       IFIFO16 fifo(.i_clk(i_clk),
+	       IFIFO16 #(DATA_WIDTH) fifo(.i_clk(i_clk),
 			    .i_data(fifo_i_item[depth][i]),
 			    .i_enq(fifo_write[depth][i]),
 			    .i_deq(fifo_read[depth][i]),
