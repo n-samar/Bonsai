d8d77a3f3d88c438adca4681570bf2b207b84b47
Nikola Samardzic <nikola.s@ucla.edu>
2019-06-25 10:36:02 -0700

    blah

---
+++ interleave/host_example.cpp
@@ -0,0 +1,368 @@
+// This is a generated file. Use and modify at your own risk.
+////////////////////////////////////////////////////////////////////////////////
+
+/*******************************************************************************
+Vendor: Xilinx
+Associated Filename: main.c
+#Purpose: This example shows a basic vector add +1 (constant) by manipulating
+#         memory inplace.
+*******************************************************************************/
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <CL/opencl.h>
+#include <CL/cl_ext.h>
+#include "xclhal2.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+#define NUM_WORKGROUPS (1)
+#define WORKGROUP_SIZE (256)
+#define MAX_LENGTH 2*8192
+#define MEM_ALIGNMENT 4096
+#if defined(SDX_PLATFORM) && !defined(TARGET_DEVICE)
+#define STR_VALUE(arg)      #arg
+#define GET_STRING(name) STR_VALUE(name)
+#define TARGET_DEVICE GET_STRING(SDX_PLATFORM)
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
+
+cl_uint load_file_to_memory(const char *filename, char **result)
+{
+    cl_uint size = 0;
+    FILE *f = fopen(filename, "rb");
+    if (f == NULL) {
+        *result = NULL;
+        return -1; // -1 means file opening fail
+    }
+    fseek(f, 0, SEEK_END);
+    size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    *result = (char *)malloc(size+1);
+    if (size != fread(*result, sizeof(char), size, f)) {
+        free(*result);
+        return -2; // -2 means file reading fail
+    }
+    fclose(f);
+    (*result)[size] = 0;
+    return size;
+}
+
+int main(int argc, char** argv)
+{
+
+    cl_int err;                            // error code returned from api calls
+    cl_uint check_status = 0;
+    const cl_uint number_of_words = 4*4096; // 16KB of data
+
+    cl_platform_id platform_id;         // platform id
+    cl_device_id device_id;             // compute device id
+    cl_context context;                 // compute context
+    cl_command_queue commands;          // compute command queue
+    cl_program program;                 // compute programs
+    cl_kernel kernel;                   // compute kernel
+
+    cl_uint* h_data;                                // host memory for input vector
+    char cl_platform_vendor[1001];
+    char target_device_name[1001] = TARGET_DEVICE;
+
+    cl_uint* h_in_ptr_output = (cl_uint*)aligned_alloc(MEM_ALIGNMENT,MAX_LENGTH * sizeof(cl_uint*)); // host memory for output vector
+    cl_mem d_in_ptr;                         // device memory used for a vector
+
+    cl_uint* h_out_ptr_output = (cl_uint*)aligned_alloc(MEM_ALIGNMENT,MAX_LENGTH * sizeof(cl_uint*)); // host memory for output vector
+    cl_mem d_out_ptr;                         // device memory used for a vector
+
+    if (argc != 2) {
+        printf("Usage: %s xclbin\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    FILE *input_file = fopen("/curr/dzonilakoni/input_file.txt", "w");
+    FILE *output_file = fopen("/curr/dzonilakoni/output_file.txt", "w");
+
+
+    // Fill our data sets with pattern
+    h_data = (cl_uint*)aligned_alloc(MEM_ALIGNMENT,MAX_LENGTH * sizeof(cl_uint*));
+    for(cl_uint i = 0; i < MAX_LENGTH; i++) {
+        h_data[i]  = i;
+        h_in_ptr_output[i] = 0; 
+        h_out_ptr_output[i] = 0; 
+
+    }
+
+    // Get all platforms and then select Xilinx platform
+    cl_platform_id platforms[16];       // platform id
+    cl_uint platform_count;
+    cl_uint platform_found = 0;
+    err = clGetPlatformIDs(16, platforms, &platform_count);
+    if (err != CL_SUCCESS) {
+        printf("Error: Failed to find an OpenCL platform!\n");
+        printf("Test failed\n");
+        return EXIT_FAILURE;
+    }
+    printf("INFO: Found %d platforms\n", platform_count);
+
+    // Find Xilinx Plaftorm
+    for (cl_uint iplat=0; iplat<platform_count; iplat++) {
+        err = clGetPlatformInfo(platforms[iplat], CL_PLATFORM_VENDOR, 1000, (void *)cl_platform_vendor,NULL);
+        if (err != CL_SUCCESS) {
+            printf("Error: clGetPlatformInfo(CL_PLATFORM_VENDOR) failed!\n");
+            printf("Test failed\n");
+            return EXIT_FAILURE;
+        }
+        if (strcmp(cl_platform_vendor, "Xilinx") == 0) {
+            printf("INFO: Selected platform %d from %s\n", iplat, cl_platform_vendor);
+            platform_id = platforms[iplat];
+            platform_found = 1;
+        }
+    }
+    if (!platform_found) {
+        printf("ERROR: Platform Xilinx not found. Exit.\n");
+        return EXIT_FAILURE;
+    }
+
+   // Get Accelerator compute device
+    cl_uint num_devices;
+    cl_uint device_found = 0;
+    cl_device_id devices[16];  // compute device id
+    char cl_device_name[1001];
+    err = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_ACCELERATOR, 16, devices, &num_devices);
+    printf("INFO: Found %d devices\n", num_devices);
+    if (err != CL_SUCCESS) {
+        printf("ERROR: Failed to create a device group!\n");
+        printf("ERROR: Test failed\n");
+        return -1;
+    }
+
+    //iterate all devices to select the target device.
+    for (cl_uint i=0; i<num_devices; i++) {
+        err = clGetDeviceInfo(devices[i], CL_DEVICE_NAME, 1024, cl_device_name, 0);
+        if (err != CL_SUCCESS) {
+            printf("Error: Failed to get device name for device %d!\n", i);
+            printf("Test failed\n");
+            return EXIT_FAILURE;
+        }
+        printf("CL_DEVICE_NAME %s\n", cl_device_name);
+        if(strcmp(cl_device_name, target_device_name) == 0) {
+            device_id = devices[i];
+            device_found = 1;
+            printf("Selected %s as the target device\n", cl_device_name);
+       }
+    }
+
+    if (!device_found) {
+        printf("Target device %s not found. Exit.\n", target_device_name);
+        return EXIT_FAILURE;
+    }
+
+    // Create a compute context
+    //
+    context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
+    if (!context) {
+        printf("Error: Failed to create a compute context!\n");
+        printf("Test failed\n");
+        return EXIT_FAILURE;
+    }
+
+    // Create a command commands
+    commands = clCreateCommandQueue(context, device_id, 0, &err);
+    if (!commands) {
+        printf("Error: Failed to create a command commands!\n");
+        printf("Error: code %i\n",err);
+        printf("Test failed\n");
+        return EXIT_FAILURE;
+    }
+
+    cl_int status;
+
+    // Create Program Objects
+    // Load binary from disk
+    unsigned char *kernelbinary;
+    char *xclbin = argv[1];
+
+    //------------------------------------------------------------------------------
+    // xclbin
+    //------------------------------------------------------------------------------
+    printf("INFO: loading xclbin %s\n", xclbin);
+    cl_uint n_i0 = load_file_to_memory(xclbin, (char **) &kernelbinary);
+    if (n_i0 < 0) {
+        printf("failed to load kernel from xclbin: %s\n", xclbin);
+        printf("Test failed\n");
+        return EXIT_FAILURE;
+    }
+
+    size_t n0 = n_i0;
+
+    // Create the compute program from offline
+    program = clCreateProgramWithBinary(context, 1, &device_id, &n0,
+                                        (const unsigned char **) &kernelbinary, &status, &err);
+    free(kernelbinary);
+
+    if ((!program) || (err!=CL_SUCCESS)) {
+        printf("Error: Failed to create compute program from binary %d!\n", err);
+        printf("Test failed\n");
+        return EXIT_FAILURE;
+    }
+
+    // Build the program executable
+    //
+    err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+    if (err != CL_SUCCESS) {
+        size_t len;
+        char buffer[2048];
+
+        printf("Error: Failed to build program executable!\n");
+        clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
+        printf("%s\n", buffer);
+        printf("Test failed\n");
+        return EXIT_FAILURE;
+    }
+
+    // Create the compute kernel in the program we wish to run
+    //
+    kernel = clCreateKernel(program, "interleave", &err);
+    if (!kernel || err != CL_SUCCESS) {
+        printf("Error: Failed to create compute kernel!\n");
+        printf("Test failed\n");
+        return EXIT_FAILURE;
+    }
+
+    // Create structs to define memory bank mapping
+    cl_mem_ext_ptr_t mem_ext;
+    mem_ext.obj = NULL;
+    mem_ext.param = kernel;
+
+
+    mem_ext.flags = 1;
+    d_in_ptr = clCreateBuffer(context,  CL_MEM_READ_WRITE | CL_MEM_EXT_PTR_XILINX,  sizeof(cl_uint) * number_of_words * 2, &mem_ext, NULL);
+
+
+    mem_ext.flags = 2;
+    d_out_ptr = clCreateBuffer(context,  CL_MEM_READ_WRITE | CL_MEM_EXT_PTR_XILINX,  sizeof(cl_uint) * number_of_words * 2, &mem_ext, NULL);
+
+
+    if (!(d_in_ptr&&d_out_ptr)) {
+        printf("Error: Failed to allocate device memory!\n");
+        printf("Test failed\n");
+        return EXIT_FAILURE;
+    }
+
+
+    err = clEnqueueWriteBuffer(commands, d_in_ptr, CL_TRUE, 0, sizeof(cl_uint) * number_of_words, h_data, 0, NULL, NULL);
+    if (err != CL_SUCCESS) {
+        printf("Error: Failed to write to source array h_data!\n");
+        printf("Test failed\n");
+        return EXIT_FAILURE;
+    }
+
+
+    err = clEnqueueWriteBuffer(commands, d_out_ptr, CL_TRUE, 0, sizeof(cl_uint) * number_of_words, h_data, 0, NULL, NULL);
+    if (err != CL_SUCCESS) {
+        printf("Error: Failed to write to source array h_data!\n");
+        printf("Test failed\n");
+        return EXIT_FAILURE;
+    }
+
+
+    // Set the arguments to our compute kernel
+    // cl_uint vector_length = MAX_LENGTH;
+    err = 0;
+    cl_uint d_insize = 0;
+    err |= clSetKernelArg(kernel, 0, sizeof(cl_uint), &d_insize); // Not used in example RTL logic.
+    err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_in_ptr); 
+    err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_out_ptr); 
+
+    if (err != CL_SUCCESS) {
+        printf("Error: Failed to set kernel arguments! %d\n", err);
+        printf("Test failed\n");
+        return EXIT_FAILURE;
+    }
+
+    // Execute the kernel over the entire range of our 1d input data set
+    // using the maximum number of work group items for this device
+
+    err = clEnqueueTask(commands, kernel, 0, NULL, NULL);
+    if (err) {
+            printf("Error: Failed to execute kernel! %d\n", err);
+            printf("Test failed\n");
+            return EXIT_FAILURE;
+        }
+
+    clFinish(commands);
+
+
+    // Read back the results from the device to verify the output
+    //
+    cl_event readevent;
+
+    err = 0;
+    err |= clEnqueueReadBuffer( commands, d_in_ptr, CL_TRUE, sizeof(cl_uint) * number_of_words, sizeof(cl_uint) * number_of_words, h_in_ptr_output, 0, NULL, &readevent );
+
+    err |= clEnqueueReadBuffer( commands, d_out_ptr, CL_TRUE, 0, sizeof(cl_uint) * number_of_words, h_out_ptr_output, 0, NULL, &readevent );
+
+
+    if (err != CL_SUCCESS) {
+            printf("Error: Failed to read output array! %d\n", err);
+            printf("Test failed\n");
+            return EXIT_FAILURE;
+        }
+    clWaitForEvents(1, &readevent);
+    // Check Results
+
+    for (cl_uint i = 0; i < number_of_words; i++) {
+        if ((h_data[i] + 1) != h_in_ptr_output[i]) {
+            printf("ERROR in interleave::m00_axi - array index %d (host addr 0x%03x) - input=%d (0x%x), output=%d (0x%x)\n", i, i*4, h_data[i], h_data[i], h_in_ptr_output[i], h_in_ptr_output[i]);
+            check_status = 1;
+        }
+        fprintf(output_file, "%d\n", h_in_ptr_output[i]);
+        fprintf(input_file, "%d\n", h_data[i]);
+      //  printf("i=%d, input=%d, output=%d\n", i,  h_in_ptr_input[i], h_in_ptr_output[i]);
+    }
+
+
+    for (cl_uint i = 0; i < number_of_words; i++) {
+        if ((h_data[i] + 1) != h_out_ptr_output[i]) {
+            printf("ERROR in interleave::m01_axi - array index %d (host addr 0x%03x) - input=%d (0x%x), output=%d (0x%x)\n", i, i*4, h_data[i], h_data[i], h_out_ptr_output[i], h_out_ptr_output[i]);
+            check_status = 1;
+        }
+      //  printf("i=%d, input=%d, output=%d\n", i,  h_out_ptr_input[i], h_out_ptr_output[i]);
+    }
+
+
+    //--------------------------------------------------------------------------
+    // Shutdown and cleanup
+    //-------------------------------------------------------------------------- 
+    clReleaseMemObject(d_in_ptr);
+    free(h_in_ptr_output);
+
+    clReleaseMemObject(d_out_ptr);
+    free(h_out_ptr_output);
+
+
+    fclose(input_file);
+    fclose(output_file);
+    free(h_data);
+    clReleaseProgram(program);
+    clReleaseKernel(kernel);
+    clReleaseCommandQueue(commands);
+    clReleaseContext(context);
+
+    if (check_status) {
+        printf("INFO: Test failed\n");
+        return EXIT_FAILURE;
+    } else {
+        printf("INFO: Test completed successfully.\n");
+        return EXIT_SUCCESS;
+    }
+
+
+} // end of main
