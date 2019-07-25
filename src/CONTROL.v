4a8646dcfa92eb92d109ece8bb69e61759867b55
Nikola Samardzic <nikola.s@ucla.edu>
2019-07-24 17:32:55 -0700

    CONTROL.v

--- src/CONTROL.v
+++ src/CONTROL.v
@@ -27,18 +27,44 @@ module CONTROL(input i_clk,
 
    // Next-state logic
    always @(*) begin
+      case(state)
+	TOGGLE: if (~i_a_min_zero & ~i_b_min_zero) begin
+	   next_state <= NOMINAL;
+	end else begin
+	   next_state <= TOGGLE;
+	end
+	DONE_A: if (i_b_min_zero) begin
+	   next_state <= TOGGLE;
+	end else begin
+	   next_state <= DONE_A;
+	end
+	DONE_B: if (i_a_min_zero) begin
+	   next_state <= TOGGLE;
+	end else begin
+	   next_state <= DONE_B;
+	end
+	NOMINAL: if (i_a_min_zero) begin
+	   next_state <= DONE_A;
+	end else if (i_b_min_zero) begin
+	   next_state <= DONE_B;
+	end else begin
+	   next_state <= NOMINAL;
+	end
+	default: next_state <= TOGGLE;
+      endcase	
+   end
+/*
       next_state <= (state == TOGGLE) ? (((~i_a_min_zero & ~i_b_min_zero) ? NOMINAL : TOGGLE)) :
 		   (state == DONE_A) ? ((i_b_min_zero ? TOGGLE : DONE_A)) :
 		   (state == DONE_B) ? ((i_a_min_zero ? TOGGLE : DONE_B)) :
 		   (state == NOMINAL) ? (i_a_min_zero ? DONE_A :
 					 (i_b_min_zero ? DONE_B : NOMINAL)) :
-		   FINISHED;
-   end
-
+		   FINISHED; 
+ */
    // Outputs
    assign stall = (i_fifo_out_full) | ((state == NOMINAL) & (i_a_empty | i_b_empty)) | (state == DONE_A & i_b_empty) | (state == DONE_B & i_a_empty) | (state == TOGGLE & (i_a_empty | ~i_a_min_zero) & (i_b_empty | ~i_b_min_zero) & (i_a_min_zero | i_b_min_zero));
-   assign switch_output = (next_state == TOGGLE & state != TOGGLE);
-   assign select_A = (next_state == NOMINAL & i_a_lte_b) | (next_state == DONE_B) | (next_state == TOGGLE & i_a_min_zero & ~i_a_empty);
+   assign switch_output = (state == DONE_A & i_b_min_zero) | (state == DONE_B & i_a_min_zero);
+   assign select_A = (((state == TOGGLE | state == NOMINAL) & (~i_a_min_zero & ~i_b_min_zero)) & i_a_lte_b) | ((state == DONE_B & ~i_a_min_zero) | (state == NOMINAL & ~i_a_min_zero & i_b_min_zero)) | (((state == DONE_A & i_b_min_zero) | (state == DONE_B & i_a_min_zero) | (state == TOGGLE & (i_a_min_zero | i_b_min_zero))) & i_a_min_zero & ~i_a_empty);
 
    initial
      begin
